045f0918f3eaa2ee5147c12ca1d3a4959f0c8237
3steak <fc.barcelone94@hotmail.fr>
2023-03-08 17:31:15 +0100

    test router

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
-header('location: /Accueil');
-die;
+// header('location: /Accueil');
+// die;
 
 
 
 
-// require_once(__DIR__ . '/router/router.php');
+require_once(__DIR__ . '/router/router.php');
 // Call class router
-// $router = new Router();
+$router = new Router();
 
 // add route for / with callable ( function call)
 // routes register in array
 //  '/' is key as url and function is the action to call
-// $router->register('/', function () {
-//     return 'HomePage';
-// });
-// $router->register('/AddPatient', function () {
-//     return 'addPatient page';
-// });
+$router->register('/', function () {
+    return  header('location: /homeCtrl.php');
+    die;
+});
+$router->register('/AddPatient', function () {
+    return 'addPatient page';
+});
+
 
 // call actions
-// $router->resolve();
+$router->resolve($_SERVER['REQUEST_URI']);
--- router/router.php
+++ router/router.php
@@ -13,5 +13,28 @@ class Router
     }
 
 
+    public function resolve(string $uri)
+    {
+
+        try {
+            // Jexplode et je conserve ce que j'ai avant '?'
+            $path = explode('?', $uri)[0];
+
+            $action = $this->routes[$path] ?? null;
+            if (!is_callable($action)) {
+                throw new Exception('Ce chemin n\'existe pas', 1);
+                die;
+            } else {
+                return $action();
+            }
+        } catch (\Throwable $th) {
+            $errorMsg = $th->getMessage();
+            include_once(__DIR__ . '/../views/templates/header.php');
+            include(__DIR__ . '/../views/errors.php');
+            include_once(__DIR__ . '/../views/templates/footer.php');
+            die;
+        }
+    }
+
     // end class
 }
