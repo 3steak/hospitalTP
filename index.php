e066060b1648cef07266d412c90438487b9fd063
3steak <fc.barcelone94@hotmail.fr>
2023-03-08 14:12:26 +0100

    enfer

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
 header('location: /Accueil');
 die;
-
-
-
-
-// require_once(__DIR__ . '/router/router.php');
-// Call class router
-// $router = new Router();
-
-// add route for / with callable ( function call)
-// routes register in array
-//  '/' is key as url and function is the action to call
-// $router->register('/', function () {
-//     return 'HomePage';
-// });
-// $router->register('/AddPatient', function () {
-//     return 'addPatient page';
-// });
-
-// call actions
-// $router->resolve();
